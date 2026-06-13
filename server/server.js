459045caaf11ac11fd403264ad9fdbefb4447346
Cshu26 <chiewshuye@gmail.com>
2026-06-13 23:41:35 +0800

    Add comments,change db version, and update docker compose configuration

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
   origin: '*', // enable any devices include mobile
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-User-Id', 'x-user-id'], // 🌟 关键：强行放行这个头部！
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-User-Id', 'x-user-id'], 
   credentials: true
 }));
 
